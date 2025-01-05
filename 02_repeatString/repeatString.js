e81c74fc9b18ea16c79bdd5fff072004ca91d596
Kenjie Tarasona <148972543+voidKing00@users.noreply.github.com>
2025-01-06 06:54:51 +0800

    Finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+const repeatString = function(text, n) {
+    
+    if(!n){
+        
+        return "";
+        
+    }else if (n <= -1){
+        
+        return "ERROR";
 
+    }
+    
+    textCopy = text;
+    n = n - 1; // first copy is considered as first iteration
+
+    for(let i = 0; i < n; i++){
+        
+        text += textCopy;
+    }
+
+    return text;
 };
 
 // Do not edit below this line
