33367ef330ecf8c49ef2c7a0d148282bc4055336
Kenjie Tarasona <148972543+voidKing00@users.noreply.github.com>
2025-01-07 05:53:15 +0800

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,40 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+
+    const originalList  = args.shift();
+    const removalList = args;
+
+    const newList = [];
+    let elementsToBeRemoved = [];
+
+    for(let i = 0; i < removalList.length; i++){
+        
+        for(let j = 0; j < originalList.length; j++){
+
+            if (removalList[i] === originalList[j]){
+                
+                elementsToBeRemoved.push(j);
+            }
+
+        }
+    }
+
+    outer:
+    for(let i = 0; i < originalList.length; i++){
+
+        for(let j = 0; j < elementsToBeRemoved.length; j++){
+
+            if (i === elementsToBeRemoved[j]){
+                
+                continue outer;
+
+            }
+        }
+
+        newList.push(originalList[i]);
+   
+    }
+    
+    return newList;
 };
 
 // Do not edit below this line
