da59a582f84c4419b8c7c08dab711406aef1f5e3
Kenjie Tarasona <148972543+voidKing00@users.noreply.github.com>
2025-01-07 04:09:26 +0800

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(text) {
 
+    let lastIndex = text.length - 1;
+    let reversedStringContainer = "";
+    
+    for(let i = lastIndex; i >= 0; i--){
+        
+        reversedStringContainer += text[i];
+
+    }
+
+    return reversedStringContainer;
 };
 
 // Do not edit below this line
