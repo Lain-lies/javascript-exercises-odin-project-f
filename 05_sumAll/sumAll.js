a3f91d358566f3c2d012b09fbd6a4cf08b245c24
Kenjie Tarasona <148972543+voidKing00@users.noreply.github.com>
2025-01-07 06:15:29 +0800

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    if (start < 0 || end < 0){
+
+        return "ERROR";
+
+    }else if(start > Math.trunc(start) || end > Math.trunc(end)){
+
+        return "ERROR";
+
+    }else if(typeof start !== 'number'  || typeof end !== 'number'){
+
+        return "ERROR";
+
+    }
+    
+    let i = start; 
+    let limit = end;
+
+    if(start > end){
+        
+        i = end;
+        limit = start;
+    }
+
+    let sum = 0;
+
+    for(;i <= limit; i++) sum += i;
+
+    return sum;
 
 };
 
+console.log(sumAll(5, 1));
 // Do not edit below this line
 module.exports = sumAll;
