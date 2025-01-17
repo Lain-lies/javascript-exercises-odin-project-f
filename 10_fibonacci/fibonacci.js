8ff83412ff4af7b1294f840546788ec5663ff885
Kenjie Tarasona <148972543+voidKing00@users.noreply.github.com>
2025-01-17 15:19:00 +0800

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
 
+    if(n < 0) return "OOPS";
+
+    //0 1 1 2 3 5
+    
+    let a = 0;
+    let b = 1;
+    let c = 0;
+
+    for(let i = 0; i < n; i++){
+        
+        c = a + b;
+        a = b;
+        b = c;
+
+    }
+    
+    return a;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
