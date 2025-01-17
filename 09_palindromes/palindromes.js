21b5a6694480ef14b4eb9e80c28ce388f6c2a07b
Kenjie Tarasona <148972543+voidKing00@users.noreply.github.com>
2025-01-17 14:39:24 +0800

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (text) {
 
+    text = text.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    /*
+
+    The regex basically means that remove all non alpha characters
+
+    ^ is the same as not ! and [a-zA-Z] lowercase and uppercase letters of the alphabet
+    
+    in pseudo it would mean: replace characters not included in the dataset[].
+
+    */
+    // console.log(text);
+    let textEnd = text.length - 1;
+    
+    for(let i = 0; i < text.length / 2; i++){
+        
+        if(text[i] !== text[textEnd - i]){
+            
+            return false;
+        }
+    }
+
+    return true;
 };
 
+// console.log(palindromes("r3ace3car"));
 // Do not edit below this line
 module.exports = palindromes;
