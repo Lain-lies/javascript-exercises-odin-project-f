e298e13c18a40aeb80c3676775d4f804ef967e06
Kenjie Tarasona <148972543+voidKing00@users.noreply.github.com>
2025-01-17 13:40:40 +0800

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function(numOne, numTwo) {
+  
+  return numOne + numTwo;
 
 };
 
-const power = function() {
-	
+const subtract = function(numOne, numTwo) {
+  
+  return numOne - numTwo;
+
 };
 
-const factorial = function() {
+const sum = function(arr) {
 	
+  return arr.reduce((total, element) => total + element, 0);
+  
+
+};
+
+const multiply = function(arr) {
+
+  return arr.reduce((total, element) => total * element);
+
+};
+
+const power = function(base, expo) {
+	
+  return base ** expo;
+
+};
+
+const factorial = function(n) {
+  
+  let total = 1;
+
+  for(let i = 1; i <= n; i++){
+    
+    total *= i;
+
+  }
+
+  return total;
+   
 };
 
 // Do not edit below this line
